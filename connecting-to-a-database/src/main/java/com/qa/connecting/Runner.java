b3dc54b358a54516a2c233207b6bdbbbc980f4dd
Naomi Gabriel <naomi_gab@hotmail.com>
2020-06-03 10:29:05 +0100

    update runner

--- connecting-to-a-database/src/main/java/com/qa/connecting/Runner.java
+++ connecting-to-a-database/src/main/java/com/qa/connecting/Runner.java
@@ -6,6 +6,7 @@ import com.qa.connecting.exceptions.ConnectionNotMadeException;
 
 public class Runner {
 	public static void main(String[] args) {
+		throw new IndexOutOfBoundsException();
 		Ims ims = new Ims();
 		ims.start();
 	}
